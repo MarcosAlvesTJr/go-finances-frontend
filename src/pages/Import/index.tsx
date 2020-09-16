4da86151b22b82e0e1549823b6785c8ca9cef5e5
Marcos Alves Trindade Junior <marcosatjr2000@gmail.com>
2020-09-15 21:28:07 -0400

    implementando  funcionalidades

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
+    const [transactionFile] = uploadedFiles;
 
-    // TODO
+    data.append('file', transactionFile.file, transactionFile.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesToBeUploaded = files.map(
+      (file): FileProps => ({
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      }),
+    );
+    setUploadedFiles(filesToBeUploaded);
   }
 
   return (
